b46182faf4d2b0ba09e7f65b9f76b8dfe31b7772
lcssoaress <lcssoaresmachado@gmail.com>
2025-09-30 21:46:31 -0300

    anotacoes

---
+++ Lista2EX3456.php
@@ -0,0 +1,28 @@
+<?php 
+echo "EX3";
+echo "<br/>";
+
+$aluno = array(
+array("Joao", 4),
+array("Lucas", 7),
+array("Rafael", 5),
+array("Guilherme", 10),
+array("Leticia", 3),
+array("Ceap", 8),
+array("kaffer", 4),
+array("Marcelly", 7),
+array("Kamilly", 6),
+array("Chaves", 10)
+);
+
+
+print_r($soma);
+
+echo "EX4";
+echo "<br/>";
+
+$meses = ["","janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"];
+
+
+print_r($meses[1]);
+ ?>

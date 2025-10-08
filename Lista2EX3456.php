f4c08f6e440cf0432563d96cdd42044b04610f02
lcssoaress <lcssoaresmachado@gmail.com>
2025-10-07 22:03:59 -0300

    primeiro commit

--- Lista2EX3456.php
+++ Lista2EX3456.php
@@ -2,22 +2,40 @@
 echo "EX3";
 echo "<br/>";
 
-$aluno = array(
-array("Joao", 4),
-array("Lucas", 7),
-array("Rafael", 5),
-array("Guilherme", 10),
-array("Leticia", 3),
-array("Ceap", 8),
-array("kaffer", 4),
-array("Marcelly", 7),
-array("Kamilly", 6),
-array("Chaves", 10)
-);
+$nota = 0;
+$maior = 0;
+$media = 0;
+$alunos = array('lucas' => 7,'marcio' => 10,'jessica' => 6,'rafael' => 4,'leticia' => 9,'pedro' => 7,'joao' => 3,'felipe' => 6,'sergio' => 9,'marcela' => 2);
 
 
-print_r($soma);
 
+
+
+foreach ($alunos as $key => $value) {
+	$media = $media + $value;
+}
+
+foreach ($alunos as $key => $value) {
+	$nota = $nota + $value;
+}
+
+for ($c=0; $c < 10; $c++) { 
+	if ($nota > $maior) {
+	$maior = $maior + $nota;
+}
+}
+
+
+
+
+$media = $media / 10;
+
+echo "A média da nota dos alunos é: $media.";
+
+echo "<br/>O aluno com maior nota é: $maior.";
+
+
+echo "<br/>";
 echo "EX4";
 echo "<br/>";
 
@@ -34,12 +52,12 @@ $cadastro = array(
 array ("joao","são paulo",18,"M"),
 array ("pedro","santos",18,"M"),
 array("joao","santos",18,"M"),
-array("joao","são paulo",18,"M"),
+array("marcia","são paulo",18,"M"),
+array("rodrigo","santos",18,"M"),
+array("fabio","são paulo",18,"M"),
+array("laercio","santos",18,"M"),
+array("felipe","são paulo",18,"M"),
 array("joao","santos",18,"M"),
-array("joao","são paulo",18,"M"),
-array("joao","são paulo",18,"M"),
-array("joao","são paulo",18,"M"),
-array("joao","são paulo",18,"M"),
 array("joao","são paulo",18,"M"),
 );
 

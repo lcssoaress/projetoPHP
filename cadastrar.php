a38c44fb5b6b8f6d3a18d3fbfa1f6f5ee05942e6
lcssoaress <lcssoaresmachado@gmail.com>
2025-10-14 20:16:50 -0300

    meu commit

--- cadastrar.php
+++ cadastrar.php
@@ -5,7 +5,14 @@ $email = $_POST['email'] ;
 if ($nome == "lucas" && $email == 'soares@gmail.com') {
 	echo("<table>");
 	echo "<tr>";
-	echo("<th><strong>cadastrado corretamente </strong></th>");
+	echo "<td>";
+	echo("<strong>cadastrado corretamente </strong>");
+	echo "</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>";
+	echo("<strong>aguarde entrada!</strong>");
+	echo "</td>";
 	echo "</tr>";
 	echo("</table>");
 }else {

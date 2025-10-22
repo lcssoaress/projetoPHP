2e8fbd66f97e77d002fa64780daf26742f4fa5ef
lcssoaress <lcssoaresmachado@gmail.com>
2025-10-21 22:16:02 -0300

    add stuff

--- include/cadastrar.php
+++ include/cadastrar.php
@@ -1,11 +1,17 @@
 <?php 
-$primeiroNome = $_POST['$primeiroNome'];
-$sobreNome = $_POST['$sobreNome'];
-$escolhaPets = $_POST['$escolhaPets'];
+if ($_SERVER['REQUEST_METHOD']== 'POST') {
+$primeiroNome = $_POST['primeiroNome'];
+$sobreNome = $_POST['sobreNome'];
+$escolhaPets = $_POST['escolhaPets'];
 
 echo "$primeiroNome "."$sobreNome "."$escolhaPets";
 
 echo '<a href="../index.php">Voltar</a>';
+	
+}else{
+	echo "é Get";
+}
+
 
 
  ?>

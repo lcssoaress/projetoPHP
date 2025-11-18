86d225f123b376b64d383998369a819b91cc7c20
lcssoaress <lcssoaresmachado@gmail.com>
2025-11-18 20:15:11 -0300

    message

---
+++ cookie/ex1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>votacao</title>
+</head>
+<body>
+	<h1>Área de votação: Vote em qual membro da akatsuki é o mais forte</h1>
+<form action="ex1.1.php">
+	Deidara:
+	<input type="radio" name="deidara">
+	Zetsu:
+	<input type="radio" name="zetsu">
+	Hidan:
+	<input type="radio" name="hidan">
+	Kakuzu:
+	<input type="radio" name="kakuzu">
+	Kisame:
+	<input type="radio" name="kisame">
+	Konan:
+	<input type="radio" name="konan">
+	Sasori:
+	<input type="radio" name="sasori">
+	Orochimaru:
+	<input type="radio" name="orochi">
+	Itachi:
+	<input type="radio" name="itachi">
+	Tobi:
+	<input type="radio" name="tobi">
+	Pain:
+	<input type="radio" name="pain">
+</form>
+</body>
+</html>

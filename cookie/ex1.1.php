75da6539cd728efb3400cbe9d62eafb50982e6c5
lcssoaress <lcssoaresmachado@gmail.com>
2025-11-18 21:47:23 -0300

    message

--- cookie/ex1.1.php
+++ cookie/ex1.1.php
@@ -1,11 +1,31 @@
 <?php 
 $escolha = $_POST["esc"];
+$deidara = 0;
+$zetsu = 0;
+$hidan = 0;
+$kakuzu = 0;
+$kisame = 0;
 setcookie("esc", $escolha, time()+60);
-if (isset($_COOKIE['esc'])== "") {
-
-	echo "Escolha um membro!";
+if ($_COOKIE['esc'] == "Deidara") {
+echo "Parabéns pelo voto!";
+	$deidara++;
+}else if ($_COOKIE['esc'] == "Zetsu") {
+echo "Parabéns pelo voto!";
+	$zetsu++;
+}else if ($_COOKIE['esc'] == "Hidan") {
+echo "Parabéns pelo voto!";
+	$hidan++;
+}else if ($_COOKIE['esc'] == "Kakuzu") {
+echo "Parabéns pelo voto!";
+	$kakuzu++;
+}else if ($_COOKIE['esc'] == "Kisame") {
+echo "Parabéns pelo voto!";
+	$kisame++;
 }else{
-	
+echo "Selecione um dos membros da akatsuki!";
+
 }
+	
+
 
  ?>
